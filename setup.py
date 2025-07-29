d8ed9f16f8817a7b57b3c531303c4f87d13183e8
Tony <anton.rogachevskyi@gmail.com>
2025-07-29 14:16:14 +0200

    tgg_common name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="common",            # именно так, чтобы при импорте было `import common`
+    name="tgg_common",      # именно так, чтобы при импорте было `import common`
     version="0.1.3",
     packages=find_packages(),  # найдёт папку common/common
     install_requires=[         # List of dependencies
